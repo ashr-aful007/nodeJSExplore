10bbdd572a0f44898e694b0012c2772a1ced1529
ashr-aful007 <ashraful99980@gmail.com>
2023-11-29 18:21:10 +0600

    add nodejsExplore

---
+++ event-emitter.js
@@ -0,0 +1,17 @@
+const EventEmiter = require("events")
+
+const myEmitter = new EventEmiter()
+
+//listener
+myEmitter.on('birthday', () =>{
+     console.log(`happy birthday to you`);
+})
+
+myEmitter.on('birthday', (gift) =>{
+     console.log(`I will send a gift ${gift}`);
+})
+
+
+
+
+myEmitter.emit('birthday', 'bike')
